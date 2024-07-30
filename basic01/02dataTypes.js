4e26e3549044d9677711261ee4f061330255e79f
DaaimKhan <113842455+DaaimKhan@users.noreply.github.com>
2024-07-30 22:27:10 +0530

    datatypes

--- basic01/02dataTypes.js
+++ basic01/02dataTypes.js
@@ -2,4 +2,18 @@
 
 // alert(2+2) // we care using node.js not browser
 
- 
+ let name = "Daaim"
+ let age = 18
+ let isLoggedIn = false
+ // number = 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true / false
+ // null (stadalone value) = it is the representation of empty value
+ // undefined = the value is not defined
+ // symbol = for use as a unique
+
+
+ // object
+ console.log(typeof undefined); // undefined
+ console.log(typeof null); // object
